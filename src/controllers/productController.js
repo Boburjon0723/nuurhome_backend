0fb46c0b3d6f194df8d6a1ef9f6eb376109802e8
Boburjon <boburjon0723@example.com>
2026-04-29 11:44:58 +0500

    asd

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = require('../lib/prisma');
 const redisClient = require('../config/redis');
 
 const CACHE_KEY_PRODUCTS = 'all_products';
